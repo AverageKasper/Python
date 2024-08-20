8fda371bfdd51513c2debc393228cebcce6d4a3c
Kasper <averagekasper@gmail.com>
2024-08-21 00:07:56 +0300

    Tehtävä 3.4 Lisätty

---
+++ Tehtävä 3/Tehtävä_3_4.py
@@ -0,0 +1,5 @@
+year = float(input("Syötä Vuosiluku: "))
+if year % 4 == 0 and year % 400 == 0:
+    print(f"Vuosi {year:.0f} on karkausvuosi.")
+else:
+    print(f"Vuosi {year:.0f} ei ole karkausvuosi.")
