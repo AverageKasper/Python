d9abc8d0ba07946b89efccd9f21203b9e94dc040
Kasper <averagekasper@gmail.com>
2024-08-21 01:47:26 +0300

    Hetki meni, Valmista tuli

--- Tehtävä 3/Tehtävä_3_4.py
+++ Tehtävä 3/Tehtävä_3_4.py
@@ -1,5 +1,13 @@
+#Kesken
 year = float(input("Syötä Vuosiluku: "))
-if year % 4 == 0 and year % 400 == 0:
-    print(f"Vuosi {year:.0f} on karkausvuosi.")
+
+if year % 4 == 0 :
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"Vuosi {year:.0f} on karkausvuosi")
+        else:
+            print(f"Vuosi {year:.0f} ei ole karkausvuosi")
+    else:
+        print(f"Vuosi {year:.0f} on karkausvuosi")
 else:
-    print(f"Vuosi {year:.0f} ei ole karkausvuosi.")
+    print(f"Vuosi {year:.0f} ei ole karkausvuosi")
