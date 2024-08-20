04b9b1b54f2eba147dc92e9fc79a096999a2b9c2
Kasper <averagekasper@gmail.com>
2024-08-20 16:06:45 +0300

    Tehtävä 2_4 lisätty

---
+++ Tehtävä 2/Tehtävä 2_4.py
@@ -0,0 +1,7 @@
+num1 = float(input("Syötä ensimmäinen numero: "))
+num2 = float(input("Syötä toinen numero: "))
+num3 = float(input("Syötä kolmas numero: "))
+summa = num1 + num2 + num3
+tulo = num1 * num2 * num3
+keskiarvo = (num1+num2+num3)/3
+print(f"Numeroiden summa on {summa:.2f}, tulo on {tulo:.2f} ja keskiarvo on {keskiarvo:.2f}")
