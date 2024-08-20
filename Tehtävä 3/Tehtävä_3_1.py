e9158639c444e64dde3920eb1b02b6a38252068c
Kasper <averagekasper@gmail.com>
2024-08-21 01:47:54 +0300

    Korjaus koodiin

--- Tehtävä 3/Tehtävä_3_1.py
+++ Tehtävä 3/Tehtävä_3_1.py
@@ -1,5 +1,5 @@
 fish_size = float(input("Kuhan koko: "))
-if fish_size > 37:
+if fish_size >= 37:
     print("Hyvän kokoinen kuha, eikun vaa syömään!")
 else:
     print("Nyt on liian pieni, heitäppä takasin mereen")
