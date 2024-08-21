c4c5c922e2f1eb8c697914adbb41b4bada72942c
Kasper <averagekasper@gmail.com>
2024-08-21 16:36:18 +0300

    Tehtävä 4.3 Lisätty

---
+++ Tehtävä 4/Tehtävä_4_3.py
@@ -0,0 +1,20 @@
+#Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi. 
+#Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
+num1 = input("Syötä ensimmäinen luku: ")
+if not num1:
+    print("Tyhjä") 
+else:
+    float(num1)
+numlist = []
+while num1:
+    numlist.append(num1)
+    num2 = input("Syötä seuraava luku: ")
+    if not num2:
+        numlist.sort()
+        print(f"Listan pienin luku on {numlist[0]} ja suurin luku on {numlist[-1]}")
+        break
+    else:
+        float(num2)
+        num1 = num2
+else:
+    print("läpi")
