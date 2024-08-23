3bbef4641a691199616a3ea87ff56ea2fd908c12
Kasper <averagekasper@gmail.com>
2024-08-23 10:14:33 +0300

    Tehtävä 3.1 korjattu

--- Tehtävä 3/Tehtävä_3_1.py
+++ Tehtävä 3/Tehtävä_3_1.py
@@ -2,4 +2,5 @@ fish_size = float(input("Kuhan koko: "))
 if fish_size >= 37:
     print("Hyvän kokoinen kuha, eikun vaa syömään!")
 else:
-    print("Nyt on liian pieni, heitäppä takasin mereen")
+    cm = 37 - fish_size
+    print(f"Nyt on {cm:.1f} senttiä liian pieni, heitäppä takasin mereen")
