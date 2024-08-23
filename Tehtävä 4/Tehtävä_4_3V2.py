654bd7be7ff108390cdc16b5ecde6fab2f4de0ea
Kasper <averagekasper@gmail.com>
2024-08-23 10:05:52 +0300

    Tehtävä 4.3 Toinen versio

---
+++ Tehtävä 4/Tehtävä_4_3V2.py
@@ -0,0 +1,21 @@
+num = input("Syötä ensimmäinen luku: ")
+if not num :
+    print("Ilman numeroa ei saa listaa aikaiseksi")
+else:
+    float(num)
+nummin = num
+nummax = num
+while num:
+    if float(num) < float(nummin):
+        nummin = num
+    elif float(num) > float(nummax):
+        nummax = num
+        print(num)
+    num = input("Syötä seuraava luku: ")
+    if not num:
+        print(f"Listan pienin luku on {nummin} ja suurin luku on {nummax}")
+        break
+    else:
+        float(num)
+else:
+    pass
