44c7426bd9605a5c4d576ba5a0bff89749de0dab
Kasper <averagekasper@gmail.com>
2024-08-23 11:28:09 +0300

    Tehtävä 4.5 Lisätty

---
+++ Tehtävä 4/Tehtävä_4_5.py
@@ -0,0 +1,24 @@
+#Kirjoita ohjelma, joka kysyy käyttäjältä käyttäjätunnuksen ja salasanan. 
+# Jos jompikumpi tai molemmat ovat väärin, tunnus ja salasana kysytään uudelleen. 
+# Tätä jatketaan kunnes kirjautumistiedot ovat oikein tai väärät tiedot on syötetty viisi kertaa. 
+# Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty. 
+# (Oikea käyttäjätunnus on python ja salasana rules).
+usercor = str('p')
+pwcor = str('r')
+attempt = 1
+while usercor and pwcor:
+    user = input("käyttäjä: ")
+    if attempt == 5:
+        print("lukittu")
+        break
+    if user == usercor:
+        pw = input("salasana: ")
+        if pw == pwcor:
+            print("sisään")
+            break
+        else:
+            attempt = attempt + 1
+            print("yritä uudestaan")
+    else:
+        attempt = attempt + 1
+        print("käyttäjä väärin")
