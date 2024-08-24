defbf1d884ac3d0c604b15e6010f553de15491e0
Kasper <averagekasper@gmail.com>
2024-08-24 03:48:18 +0300

    Tehtävä 5.1 Lisätty

---
+++ Tehtävä 5/Tehtävä_5_1.py
@@ -0,0 +1,13 @@
+import random as r
+dice_amount = int(input("Montako noppaa heitetään: "))
+dice = []
+score = 0
+while dice_amount > 0:
+    roll = r.randint(1,6)
+    dice.append(roll)
+    dice_amount = dice_amount - 1
+for n in dice:
+     score = score + n 
+
+print(dice)
+print(f"Noppien kokonais tulo : {score}")
