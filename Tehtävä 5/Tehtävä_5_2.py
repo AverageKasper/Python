22fd63a73e8fa666379fea17cf7a53d54b880a28
Kasper <averagekasper@gmail.com>
2024-08-24 05:09:38 +0300

    Tehtävä 5.2 Lisätty

---
+++ Tehtävä 5/Tehtävä_5_2.py
@@ -0,0 +1,24 @@
+#Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi. 
+# Lopuksi ohjelma tulostaa saaduista luvuista viisi suurinta suuruusjärjestyksessä suurimmasta alkaen. 
+# Vihje: listan alkioiden lajittelujärjestyksen voi kääntää antamalla sort-metodille argumentiksi reverse=True.
+num1 = input("Syötä ensimmäinen luku: ")
+if not num1:
+    print("Ilman numeroa ei saa listaa aikaiseksi") 
+else:
+    float(num1)
+numlist = []
+amount = 0
+while num1:
+    numlist.append(num1)
+    num2 = input("Syötä seuraava luku: ")
+    if not num2:
+        numlist.sort(key=float,reverse=True)
+        break
+    else:
+        float(num2)
+        num1 = num2
+print("Suurimmat luvut:")
+for n in numlist:
+    if amount < 5:
+        print (n)
+        amount = amount + 1
