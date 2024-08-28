cc93c2848a915aa13a2a48506233235ee6a953d1
Kasper <averagekasper@gmail.com>
2024-08-28 13:43:31 +0300

    Tehtävän anto luettu kunnolla, bonus feature heitto counter

--- Tehtävä 6/Tehtävä_6_1.py
+++ Tehtävä 6/Tehtävä_6_1.py
@@ -1,7 +1,11 @@
 #Kirjoita parametriton funktio, joka palauttaa paluuarvonaan satunnaisen nopan silmäluvun väliltä 1..6. 
 # Kirjoita pääohjelma, joka heittää noppaa niin kauan kunnes tulee kuutonen. 
 # Pääohjelma tulostaa kunkin heiton jälkeen saadun silmäluvun.
+
+# Lisäsin heitto counterin extrana
+
 import random as r
+throw = 1
 
 def dice():
     dice_roll = r.randint(1,6)
@@ -12,5 +16,6 @@ final_roll = dice()
 while final_roll != 6:
     print(f"Nopan tulos on {final_roll}, heitetään uudestaan")
     final_roll = dice()
+    throw += 1
 else:
-    print(f"Lopputulos {final_roll}")
+    print(f"Lopputulos {final_roll}, saavutettu {throw} heitossa")
--- Tehtävä 6/Tehtävä_6_2.py
+++ Tehtävä 6/Tehtävä_6_2.py
@@ -2,15 +2,19 @@
 # Muokatun funktion avulla voit heitellä esimerkiksi 21-tahkoista roolipelinoppaa. 
 # Edellisestä tehtävästä poiketen nopan heittelyä jatketaan pääohjelmassa kunnes saadaan nopan maksimisilmäluku, 
 # joka kysytään käyttäjältä ohjelman suorituksen alussa.#
+
+# Lisäsin heitto counterin extrana
 import random as r
-dice_sides = int(input("Nopan tahkomäärä: "))
+dice_sides = int(input("Syötä nopan tahkomäärä: "))
+throw = 1
 def dice(sides):
     dice_roll = r.randint(1,sides)
-    if dice_roll != sides:
-        print(f"Tulos on {dice_roll}, heitetään uudestaan")
-        dice(dice_sides)
-    elif dice_roll == sides:
-        return()
+    return(dice_roll)
 
-dice(dice_sides)
-print(f"Noppa tulos on {dice_sides}, yippee")
+final_roll = dice(dice_sides)
+while final_roll != dice_sides:
+    print(f"Tulos oli {final_roll}, heitetään uudestaan")
+    final_roll = dice(dice_sides)
+    throw += 1
+else:
+    print(f"Nopan tulos on {final_roll}. Maksimi luku saavutettu {throw} heitossa")
