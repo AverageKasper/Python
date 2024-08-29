42563d1c672335f8e2719ddf04dbfb0b32b52e08
Kasper <averagekasper@gmail.com>
2024-08-29 14:23:19 +0300

    Lista pidennetty

--- Tehtävä 6/Tehtävä_6_4.py
+++ Tehtävä 6/Tehtävä_6_4.py
@@ -2,7 +2,7 @@
 # Ohjelma palauttaa listassa olevien lukujen summan. 
 # Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota ja tulostat sen palauttaman summan.
 
-numlist = [1,2,3,4,5,6]
+numlist = [1,2,3,4,5,6,7,8,9]
 
 def laskuri(num):
     score = 0
