62437aec5b291dc4a90321aa5ae5e6f5f681c770
Kasper <averagekasper@gmail.com>
2024-09-01 20:29:40 +0300

    Maa muutettu kaupungiksi, en tiedä onko oikein

--- Tehtävä 8/Tehtävä_8_1.py
+++ Tehtävä 8/Tehtävä_8_1.py
@@ -1,13 +1,13 @@
 import mysql.connector
 
 def airport_finder(icao):
-    sql = f"select airport.name as 'airport name', country.name as 'country name' from airport,country where airport.iso_country = country.iso_country and airport.ident = '{icao}';"
+    sql = f"select name, municipality from airport where airport.ident = '{icao}';"
     kursori = yhteys.cursor()
     kursori.execute(sql)
     tulos = kursori.fetchall()
     if kursori.rowcount >0:
         for rivi in tulos:
-            print(f"Lentokentän nimi on {rivi[0]}, ja maa on {rivi[1]}")
+            print(f"Lentokentän nimi on {rivi[0]}, kaupungista {rivi[1]}")
 
 yhteys = mysql.connector.connect(
          host='localhost',
