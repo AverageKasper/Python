bba778b04b5c603e65928d2f1ee12868b72b92b7
Kasper <averagekasper@gmail.com>
2024-09-03 10:43:15 +0300

    8.1 päivitetty siirsin sql yhteyden ylemmäs, näyttää paremmalta omasta mielestä

--- Tehtävä 8/Tehtävä_8_1.py
+++ Tehtävä 8/Tehtävä_8_1.py
@@ -1,14 +1,5 @@
 import mysql.connector
 
-def airport_finder(icao):
-    sql = f"select name, municipality from airport where airport.ident = '{icao}';"
-    kursori = yhteys.cursor()
-    kursori.execute(sql)
-    tulos = kursori.fetchall()
-    if kursori.rowcount >0:
-        for rivi in tulos:
-            print(f"Lentokentän nimi on {rivi[0]}, kaupungista {rivi[1]}")
-
 yhteys = mysql.connector.connect(
          host='localhost',
          database='flight_game',
@@ -17,6 +8,15 @@ yhteys = mysql.connector.connect(
          autocommit=True,
          collation="utf8mb4_general_ci"
          )
+
+def airport_finder(icao):
+    sql = f"select name, municipality from airport where airport.ident = '{icao}';"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount >0:
+        for rivi in tulos:
+            print(f"Lentokentän nimi on {rivi[0]}, kaupungista {rivi[1]}")
 
 icao = input("Syötä lentokentän ICAO-koodi: ")
 airport_finder(icao)
