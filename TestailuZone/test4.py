c8d759ff5d201d0f5136b4a1c498f0eca92fff91
AverageKasper <averagekasper@gmail.com>
2024-09-04 23:50:54 +0300

    HUH

--- TestailuZone/test4.py
+++ TestailuZone/test4.py
@@ -1,1 +1,11 @@
-pass
+class Car:
+    def __init__(monke, register, top_speed,):
+        monke.register = register
+        monke.top_speed = top_speed
+        monke.current_speed = 0
+        monke.distance = 0
+
+charger = Car("ABC-123","142 km/h")
+
+print(f"Auton ominaisuudet:\nrekisteritunnus: {charger.register} \nhuippunopeus: {charger.top_speed} "
+      f"\ntämänhetkinen nopeus: {charger.current_speed} \nkuljettu matka: {charger.distance}")
