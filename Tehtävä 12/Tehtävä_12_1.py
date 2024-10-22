176395f61c64e74c3453ee9e04bb300ab51477e9
AverageKasper <averagekasper@gmail.com>
2024-10-22 18:30:00 +0300

    Tehtävä 12.1 tehty

---
+++ Tehtävä 12/Tehtävä_12_1.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+
+answer = input("Would you like to read a chuck norris joke? (Y/N) ").upper().strip()
+while answer != "Y" and answer != "N":
+    answer = input("Invalid input, try again. (Y/N) ")
+while answer != "N":
+    try:
+        joke = "https://api.chucknorris.io/jokes/random"
+        result = requests.get(joke).json()
+
+        print(result["value"])
+
+    except requests.exceptions.RequestException as e:
+        print("Could not complete request")
+    answer = input("Would you like to read another chuck norris joke? (Y/N) ").upper().strip()
+    while answer != "Y" and answer != "N":
+        answer = input("Invalid input, try again. (Y/N) ")
+print("Goodbye!")
