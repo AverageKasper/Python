665c16e4d48578a4f24a9f5ca4d29de97630a5fb
AverageKasper <averagekasper@gmail.com>
2024-10-24 12:53:31 +0300

    matikka päivitetty

--- Tehtävä 12/Tehtävä_12_2.py
+++ Tehtävä 12/Tehtävä_12_2.py
@@ -4,7 +4,6 @@ import json
 api = "d1187dec0bdb32d1a030a27b739d24a2"
 city_name = input("What city are you intrested?\n")
 
-
 try:
     weather_request = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api}"
     weather_result = requests.get(weather_request).json()
