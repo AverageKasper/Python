4d2b1a4d7a96262c4182fcdae301685f4b864ab9
AverageKasper <averagekasper@gmail.com>
2024-10-24 17:36:13 +0300

    matikka päivitetty final

--- Matikka_Python/Mat_Tehtävä.py
+++ Matikka_Python/Mat_Tehtävä.py
@@ -13,6 +13,12 @@ print(f"Tehtävä 2 b) {np.radians(d)}\n")
 list = np.array([30, 45, 60, 90, 120, 135, 150, 180, 270, 360])
 
 print("Tehtävä 3 lista")
-
+print("_" * 33)
 for n in list:
-    print(str(np.radians(n)) + " rad")
+    print(f"| Degree: {n:3} | Radians: {np.radians(n):4.4f} |")
+print("¯" * 33) 
+
+print("Tehtävä 2")
+# En kirjoita suomeksi, my bad
+print(f"Triangles sides are 1.6m and 2.3m. Triangles hypotenuse size: {np.hypot(1.6, 2.3):.2f}m")
+
